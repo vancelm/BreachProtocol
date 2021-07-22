59dc726910f33c56d73535ed75818201315871c2
Vance Morgan <vance@vancemorgan.com>
2021-07-22 16:21:55 -0500

    Added PuzzleItem.cs

---
+++ src/BreachProtocol.Core/PuzzleItem.cs
@@ -0,0 +1,16 @@
+﻿namespace BreachProtocol
+{
+    internal struct PuzzleItem
+    {
+        public readonly int Row;
+        public readonly int Column;
+        public readonly byte Value;
+
+        internal PuzzleItem(int row, int column, byte value)
+        {
+            Row = row;
+            Column = column;
+            Value = value;
+        }
+    }
+}
