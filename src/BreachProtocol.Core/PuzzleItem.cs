6afca0e02dc9f6e0e2e458a86b0de0890b3da96f
Vance Morgan <vance@vancemorgan.com>
2021-07-23 07:55:46 -0500

    Changed access modifers to public

--- src/BreachProtocol.Core/PuzzleItem.cs
+++ src/BreachProtocol.Core/PuzzleItem.cs
@@ -3,22 +3,22 @@
     /// <summary>
     /// Represents a value from a row and column position in a matrix.
     /// </summary>
-    internal struct PuzzleItem
+    public struct PuzzleItem
     {
         /// <summary>
         /// Gets the row of this <see cref="PuzzleItem"/>.
         /// </summary>
-        internal readonly int Row;
+        public readonly int Row;
 
         /// <summary>
         /// Gets the column of this <see cref="PuzzleItem"/>.
         /// </summary>
-        internal readonly int Column;
+        public readonly int Column;
 
         /// <summary>
         /// Gets the value of this <see cref="PuzzleItem"/>.
         /// </summary>
-        internal readonly byte Value;
+        public readonly byte Value;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PuzzleItem"/> struct with the specified row, column, and value.
@@ -26,7 +26,7 @@
         /// <param name="row">The row of this <see cref="PuzzleItem"/></param>
         /// <param name="column">The column of this <see cref="PuzzleItem"/></param>
         /// <param name="value">The value of this <see cref="PuzzleItem"/></param>
-        internal PuzzleItem(int row, int column, byte value)
+        public PuzzleItem(int row, int column, byte value)
         {
             Row = row;
             Column = column;
