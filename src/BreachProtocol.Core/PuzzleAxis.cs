b365543fd7ef526a5fe9bb76297423d61115ad84
Vance Morgan <vance@vancemorgan.com>
2021-07-23 03:47:46 -0500

    Added documentation

--- src/BreachProtocol.Core/PuzzleAxis.cs
+++ src/BreachProtocol.Core/PuzzleAxis.cs
@@ -1,8 +1,18 @@
 ﻿namespace BreachProtocol
 {
+    /// <summary>
+    /// Represents the axis for limiting moves in a breach protocol puzzle.
+    /// </summary>
     public enum PuzzleAxis
     {
+        /// <summary>
+        /// Represents a horizontal axis allowing only column moves.
+        /// </summary>
         Horizontal = 0,
+
+        /// <summary>
+        /// Represents a vertical axis allowing only row moves.
+        /// </summary>
         Vertical = 1
     }
 }
